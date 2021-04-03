0e8d585bdee9167fe278f61e86f55162432f56c3
Oliver <oliverkozul@gmail.com>
2021-04-03 18:27:43 +0200

    Finalan

--- app.js
+++ app.js
@@ -77,9 +77,9 @@ app.post("/api/login", async (req, res) => {
         });
     }
 });
-app.get("/check-session", (req,res,next) => {
+app.get("/api/getUser", (req,res,next) => {
     console.log.apply(req.session);
-    res.json({user: req.session.user});
+    res.json({username: req.session.user.username});
 });
 /*app.get("/api/logout", function(req, res){
     req.logout();
@@ -156,8 +156,10 @@ app.get("/api/event", async (req, res) => {
 app.post("/api/events", async (req, res) => {
     try {
         const name = req.body.name;
+        //const organizer = req.body.organizer;
         const type = req.body.type;
         const gameroom = req.body.gameroom;
+        //const attendees = req.body.attendees;
         const desc = req.body.desc;
 
         const newEvent = new event({
--- front/js/createEvent.js
+++ front/js/createEvent.js
@@ -3,14 +3,14 @@ addButton.addEventListener("click", GetInput);
 
 async function GetInput() {
     const nameInput = document.querySelector("#name-input");
-    //const organizerInput = document.querySelector("#organizer-input");
+    // const organizerInput = await axios.get("api/getUser", user);
     const radionica = document.querySelector("#radionica");
-    const gameroomInput = document.querySelector("#gameroom-input");
+    const gameroomInput = document.querySelector("#gameroomChoose");
     //const attendeesInput = document.querySelector("#attendees-input");
     const descInput = document.querySelector("#desc-input");
 
     const name = nameInput.value;
-    //const organizer = organizerInput.value;
+    //const organizer = organizerInput.body.username;
     let type;
     if(radionica.value)
         type="radionica";
